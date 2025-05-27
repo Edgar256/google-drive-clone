29a191ed75922d7a562f568908f8c62e3f269554
Edgar Tinkamanyire <tinka.edgar@gmail.com>
2025-05-27 23:29:56 +0300

    Deployment import error fixes

--- app/api/files/[fileId]/share/route.ts
+++ app/api/files/[fileId]/share/route.ts
@@ -1,3 +1,10 @@
+import { NextResponse } from "next/server";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../../../lib/auth";
 import { PrismaClient } from "@prisma/client";
+
 const prisma = new PrismaClient();
-export default prisma; 
+
+export async function POST(req: Request) {
+  // ... existing code ...
+} 
